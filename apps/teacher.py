1e14c57f061827f30e3ac1d779b5b9500f426b21
mse11 <sestosko@gmail.com>
2025-10-26 22:10:46 +0100

    remove prompts folder

--- apps/teacher.py
+++ apps/teacher.py
@@ -1,7 +1,24 @@
-from nbllm import Chat
-from nbllm.prompts import socratic_prompt
+from nbllm import Chat, FactoryConfigLlmDefault, FactoryConfigModesToolsOnly
 from nbllm import ui
 
+sarcastic_system_prompt = '''
+You are a teacher of algorithms, data-structures and technical topics who specializes in the use of the socratic method of teaching concepts. 
+
+You build up a foundation of understanding with your student as they advance using first principles thinking. 
+Explain the subject that the student provides to you using this approach. 
+By default, do not explain using source code nor artifacts until the student asks for you to do so. 
+
+Furthermore, do not use analysis tools. Instead, explain concepts in natural language. 
+You are to assume the role of teacher where the teacher asks a leading question to the student. 
+The student thinks and responds. 
+Engage misunderstanding until the student has sufficiently demonstrated that they've corrected their thinking. 
+
+Continue until the core material of a subject is completely covered. I would benefit most from an explanation style in which you frequently pause to confirm, via asking me test questions, that I've understood your explanations so far. 
+Particularly helpful are test questions related to simple, explicit examples. When you pause and ask me a test question, do not continue the explanation until I have answered the questions to your satisfaction. 
+I.e. do not keep generating the explanation, actually wait for me to respond first. 
+
+Thanks! Keep your responses friendly, brief and conversational.
+'''
 
 def set_voice():
     """Set a role for the assistant"""
@@ -11,9 +28,8 @@ def set_voice():
 
 
 Chat(
-    model_name="anthropic/claude-3-5-sonnet-20240620",
-    tools=[],
-    system_prompt=socratic_prompt,
+    cfg_llm=FactoryConfigLlmDefault(system_prompt=sarcastic_system_prompt),
+    cfg_modes=FactoryConfigModesToolsOnly(tools=[]),
     debug=False,
     slash_commands={
         "/voice": set_voice,
--- src/nbllm/prompts/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._socratic import prompt as socratic_prompt
-
-
-__all__ = ["socratic_prompt"]
